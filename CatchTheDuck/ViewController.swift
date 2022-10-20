418c373d3d028d49510805194c62e853147b23f7
Sinan Selek <sselek62@gmail.com>
2022-10-20 18:14:27 +0300

    Initial Commit

---
+++ CatchTheDuck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheDuck
+//
+//  Created by Sinan Selek on 20.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
